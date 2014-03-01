38762ad59f1d0474fae2743412d6e3b1a3dfee9f
Anojh Gnanachandran <anojhgnanachandran@gmail.com>
2014-03-01 12:41:14 -0500

    Only allow certain user names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,25 +1,27 @@
 class User < ActiveRecord::Base
-	has_secure_password
+  has_secure_password
   has_many :places
 
   extend FriendlyId
   friendly_id :name, use: :slugged
 
-	validates :name, presence: true, length: { maximum: 50 }, uniqueness: true
-	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, 
-				format: { with: VALID_EMAIL_REGEX }, 
-				uniqueness: { case_sensitive: false }
-	validates :password, length: { minimum: 6 } # BCrypt adds a password presence validation on default. Adding one here creates two similar error messages when the password field is empty on submission
-	validates :password_confirmation, presence: true
+  VALID_NAME_REGEX = /\A[\w\-_]+\z/
+  validates :name, presence: true, format: { with: VALID_NAME_REGEX }, length: { maximum: 50 }, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, 
+    format: { with: VALID_EMAIL_REGEX }, 
+    uniqueness: { case_sensitive: false }
+  validates :password, length: { minimum: 6 } # BCrypt adds a password presence validation on default. Adding one here creates two similar error messages when the password field is empty on submission
+  validates :password_confirmation, presence: true
 
-	# to ensure that non unique emails are also caught at the database level
-	# see migration: add_index_to_users_email
-	before_save { |user| user.email = email.downcase }
-	before_save :create_remember_token
+  # to ensure that non unique emails are also caught at the database level
+  # see migration: add_index_to_users_email
+  before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
 
-	private
-		def create_remember_token
-			self.remember_token = SecureRandom.urlsafe_base64
-		end
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+    end
+
 end
